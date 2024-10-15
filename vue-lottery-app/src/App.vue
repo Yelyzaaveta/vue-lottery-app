044da590953730efd1694cab580914ca19717527
Yelyzaaveta <121977737+Yelyzaaveta@users.noreply.github.com>
2024-10-15 12:45:41 +0300

    add filtering and sorting

--- vue-lottery-app/src/App.vue
+++ vue-lottery-app/src/App.vue
@@ -7,11 +7,12 @@
           @remove-winner="removeWinner"
       />
       <RegistrationForm 
-            :participants="participants"
-            :winners="winners" 
-            @add-participant="addParticipant"
-        />
-      <ParticipantsTable :participants="participants" />
+          :participants="participants"
+          :winners="winners" 
+          @add-participant="addParticipant"
+      />
+      <SearchBar @filter-by-name="filterParticipants" />
+      <ParticipantsTable :participants="filteredParticipants" />
   </div>
 </template>
 
@@ -20,19 +21,31 @@ import { defineComponent, ref } from 'vue';
 import RegistrationForm from './components/FormComponent.vue';
 import WinnersList from './components/WinnersListComponent.vue';
 import ParticipantsTable from './components/ParticipantsComponent.vue';
+import SearchBar from './components/SearchBar.vue';
 
 export default defineComponent({
   components: {
       RegistrationForm,
       WinnersList,
+      SearchBar,
       ParticipantsTable
   },
+  
   setup() {
       const winners = ref<{ name: string }[]>([]);
       const participants = ref<{ name: string; birthday: string; email: string; phone: string }[]>([]);
+      const filteredParticipants = ref<{ name: string; birthday: string; email: string; phone: string }[]>(participants.value);
 
       const addParticipant = (participant: { name: string; birthday: string; email: string; phone: string }) => {
           participants.value.push(participant);
+          filteredParticipants.value = participants.value; // Скидаємо фільтр після додавання
+      };
+
+      const filterParticipants = (filter: string) => {
+          const regex = new RegExp(filter, 'i');
+          filteredParticipants.value = participants.value.filter(participant =>
+              regex.test(participant.name)
+          );
       };
 
       const addWinner = (winner: { name: string }) => {
@@ -46,10 +59,13 @@ export default defineComponent({
       return {
           winners,
           participants,
+          filteredParticipants,
           addParticipant,
+          filterParticipants,
           addWinner,
           removeWinner
       };
+      
   }
 });
 </script>
--- vue-lottery-app/src/components/ParticipantsComponent.vue
+++ vue-lottery-app/src/components/ParticipantsComponent.vue
@@ -1,36 +1,67 @@
 <template>
+  
   <table class="min-w-full bg-white shadow-md rounded">
-      <thead>
-          <tr>
-              <th class="px-4 py-2">#</th>
-              <th class="px-4 py-2">Name</th>
-              <th class="px-4 py-2">Date of Birth</th>
-              <th class="px-4 py-2">Email</th>
-              <th class="px-4 py-2">Phone number</th>
-          </tr>
-      </thead>
-      <tbody>
-          <tr v-for="(participant, index) in participants" :key="index">
-              <td class="border px-4 py-2">{{ index + 1 }}</td>
-              <td class="border px-4 py-2">{{ participant.name }}</td>
-              <td class="border px-4 py-2">{{ participant.birthday }}</td>
-              <td class="border px-4 py-2">{{ participant.email }}</td>
-              <td class="border px-4 py-2">{{ participant.phone }}</td>
-          </tr>
-      </tbody>
+    <thead>
+     
+
+      <tr>
+        <th class="px-4 py-2">#</th>
+        <th class="px-4 py-2">Name</th>
+        <th class="px-4 py-2">Date of Birth</th>
+        <th class="px-4 py-2">Email</th>
+        <th class="px-4 py-2">Phone number</th>
+        <th class="px-4 py-2">Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(participant, index) in participants" :key="index">
+        <td class="border px-4 py-2">{{ index + 1 }}</td>
+        <td class="border px-4 py-2">{{ participant.name }}</td>
+        <td class="border px-4 py-2">{{ participant.birthday }}</td>
+        <td class="border px-4 py-2">{{ participant.email }}</td>
+        <td class="border px-4 py-2">{{ participant.phone }}</td>
+        
+      
+      </tr>
+    </tbody>
+    
   </table>
+  <div class="w-[600px] flex flex-row">
+        <div class="px-4 py-2 flex flex-row">
+          <p>Sort By Name</p>
+        <button @click="sortByName" class="ml-2">A-Z</button>
+      </div>
+      <div class="px-4 py-2 flex flex-row">
+        <p>Date of Birth</p>
+        <button @click="sortByBirthday" class="ml-2">/\</button>
+        <button @click="sortByBirthdayDown" class="ml-2">\/</button>
+      </div>
+      </div>
 </template>
 
 <script lang="ts">
-import { defineComponent} from 'vue';
-import type {  PropType } from 'vue';
+import { defineComponent } from 'vue';
+import type { PropType } from 'vue';
 
 export default defineComponent({
   props: {
-      participants: {
-          type: Array as PropType<{ name: string; birthday: string; email: string; phone: string }[]>,
-          required: true,
-      },
+    participants: {
+      type: Array as PropType<{ name: string; birthday: string; email: string; phone: string }[]>,
+      required: true,
+    },
   },
+  methods: {
+    sortByName() {
+        this.participants.sort((a, b) => a.name.localeCompare(b.name));
+    },
+    sortByBirthday() {
+        this.participants.sort((a, b) => new Date(a.birthday).getTime() - new Date(b.birthday).getTime());
+    },
+    sortByBirthdayDown() {
+        this.participants.sort((a, b) => new Date(b.birthday).getTime() - new Date(a.birthday).getTime());
+    },
+    
+}
+
 });
 </script>
