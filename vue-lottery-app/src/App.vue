42e11b010eb9882c894d8cab73847435a47fcde3
Yelyzaaveta <121977737+Yelyzaaveta@users.noreply.github.com>
2024-10-29 00:26:52 +0200

    add Esc and enter

--- vue-lottery-app/src/App.vue
+++ vue-lottery-app/src/App.vue
@@ -1,71 +1,83 @@
 <template>
-  <div class="max-w-3xl mx-auto mt-[60px]">
-      <WinnersList 
-          :winners="winners"
-          :participants="participants"
-          @select-winner="addWinner"
-          @remove-winner="removeWinner"
-      />
-      <RegistrationForm 
-          :participants="participants"
-          :winners="winners" 
-          @add-participant="addParticipant"
-      />
-      <SearchBar @filter-by-name="filterParticipants" />
-      <ParticipantsTable :participants="filteredParticipants" />
-  </div>
-</template>
-
-<script lang="ts">
-import { defineComponent, ref } from 'vue';
-import RegistrationForm from './components/FormComponent.vue';
-import WinnersList from './components/WinnersListComponent.vue';
-import ParticipantsTable from './components/ParticipantsComponent.vue';
-import SearchBar from './components/SearchBar.vue';
-
-export default defineComponent({
-  components: {
-      RegistrationForm,
-      WinnersList,
-      SearchBar,
-      ParticipantsTable
-  },
+    <div class="max-w-3xl mx-auto mt-[60px]">
+        <WinnersList 
+            :winners="winners"
+            :participants="participants" 
+            @select-winner="addWinner"
+            @remove-winner="removeWinner"
+        />
+        <RegistrationForm 
+            :participants="participants"
+            :winners="winners" 
+            @add-participant="addParticipant"
+        />
+        <SearchBar @filter-by-name="filterParticipants" />
+        <ParticipantsTable :participants="filteredParticipants" />
+    </div>
+  </template>
   
-  setup() {
-      const winners = ref<{ name: string }[]>([]);
-      const participants = ref<{ name: string; birthday: string; email: string; phone: string }[]>([]);
-      const filteredParticipants = ref<{ name: string; birthday: string; email: string; phone: string }[]>(participants.value);
-
-      const addParticipant = (participant: { name: string; birthday: string; email: string; phone: string }) => {
-          participants.value.push(participant);
-          filteredParticipants.value = participants.value; // Скидаємо фільтр після додавання
-      };
-
-      const filterParticipants = (filter: string) => {
-          const regex = new RegExp(filter, 'i');
-          filteredParticipants.value = participants.value.filter(participant =>
-              regex.test(participant.name)
-          );
-      };
-
-      const addWinner = (winner: { name: string }) => {
-          winners.value.push(winner);
-      };
-
-      const removeWinner = (index: number) => {
-          winners.value.splice(index, 1);
-      };
-
-      return {
-          winners,
-          participants,
-          filteredParticipants,
-          addParticipant,
-          filterParticipants,
-          addWinner,
-          removeWinner
-      };
-      
-  }
-});
-</script>
+  <script lang="ts">
+  import { defineComponent, ref, onMounted, watch } from 'vue';
+  import RegistrationForm from './components/FormComponent.vue';
+  import WinnersList from './components/WinnersListComponent.vue';
+  import ParticipantsTable from './components/ParticipantsComponent.vue';
+  import SearchBar from './components/SearchBar.vue';
+  
+  export default defineComponent({
+    components: {
+        RegistrationForm,
+        WinnersList,
+        SearchBar,
+        ParticipantsTable
+    },
+    
+    setup() {
+        const winners = ref<{ name: string }[]>([]);
+        const participants = ref<{ name: string; birthday: string; email: string; phone: string }[]>([]);
+        const filteredParticipants = ref<{ name: string; birthday: string; email: string; phone: string }[]>([]);
+  
+        onMounted(() => {
+            const storedParticipants = localStorage.getItem('participants');
+            if (storedParticipants) {
+                participants.value = JSON.parse(storedParticipants);
+                filteredParticipants.value = participants.value; // Встановлюємо відфільтровані учасники
+            }
+        });
+  
+        watch(participants, (newVal) => {
+            localStorage.setItem('participants', JSON.stringify(newVal));
+        }, { deep: true });
+  
+        const addParticipant = (participant: { name: string; birthday: string; email: string; phone: string }) => {
+            participants.value.push(participant);
+            filteredParticipants.value = participants.value; // Скидаємо фільтр після додавання
+        };
+  
+        const filterParticipants = (filter: string) => {
+            const regex = new RegExp(filter, 'i');
+            filteredParticipants.value = participants.value.filter(participant =>
+                regex.test(participant.name)
+            );
+        };
+  
+        const addWinner = (winner: { name: string }) => {
+            winners.value.push(winner);
+        };
+  
+        const removeWinner = (index: number) => {
+            winners.value.splice(index, 1);
+        };
+  
+        return {
+            winners,
+            participants,
+            filteredParticipants,
+            addParticipant,
+            filterParticipants,
+            addWinner,
+            removeWinner
+        };
+    }
+  });
+  </script>
+  
--- vue-lottery-app/src/components/ParticipantsComponent.vue
+++ vue-lottery-app/src/components/ParticipantsComponent.vue
@@ -1,9 +1,6 @@
 <template>
-  
   <table class="min-w-full bg-white shadow-md rounded">
     <thead>
-     
-
       <tr>
         <th class="px-4 py-2">#</th>
         <th class="px-4 py-2">Name</th>
@@ -20,48 +17,90 @@
         <td class="border px-4 py-2">{{ participant.birthday }}</td>
         <td class="border px-4 py-2">{{ participant.email }}</td>
         <td class="border px-4 py-2">{{ participant.phone }}</td>
-        
-      
+        <td class="border px-4 py-2">
+          <button @click="openEditModal(participant)" class="text-blue-500">_/</button>
+          <button @click="openDeleteModal(participant)" class="text-red-500 ml-4">X</button>
+        </td>
       </tr>
     </tbody>
-    
   </table>
-  <div class="w-[600px] flex flex-row">
-        <div class="px-4 py-2 flex flex-row">
-          <p>Sort By Name</p>
-        <button @click="sortByName" class="ml-2">A-Z</button>
-      </div>
-      <div class="px-4 py-2 flex flex-row">
-        <p>Date of Birth</p>
-        <button @click="sortByBirthday" class="ml-2">/\</button>
-        <button @click="sortByBirthdayDown" class="ml-2">\/</button>
-      </div>
-      </div>
+
+  <Modal v-if="showEditModal" :isOpen="showEditModal" title="Edit Participant Data" confirmButtonText="Save"
+    @close="closeEditModal" @confirm="updateParticipant">
+    <form>
+      <input v-model="editedParticipant.name" placeholder="Name" class="w-full p-2 border mb-2" />
+      <input v-model="editedParticipant.birthday" type="date" placeholder="Date of Birth"
+        class="w-full p-2 border mb-2" />
+      <input v-model="editedParticipant.email" placeholder="Email" class="w-full p-2 border mb-2" />
+      <input v-model="editedParticipant.phone" placeholder="Phone number" class="w-full p-2 border mb-2" />
+    </form>
+  </Modal>
+
+  
+  <Modal v-if="showDeleteModal" :isOpen="showDeleteModal" title="Confirm Delete" confirmButtonText="Delete"
+    confirmButtonClass="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600" @close="closeDeleteModal"
+    @confirm="confirmDeleteParticipant">
+    <p>Are you sure you want to delete {{ deletedParticipant?.name }} ?</p>
+  </Modal>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import type { PropType } from 'vue';
+import { defineComponent, onMounted, onUnmounted, ref } from 'vue';
+import Modal from './ModalComponent.vue';
 
 export default defineComponent({
+  components: { Modal },
   props: {
     participants: {
-      type: Array as PropType<{ name: string; birthday: string; email: string; phone: string }[]>,
+      type: Array,
       required: true,
     },
   },
+  data() {
+    return {
+      showEditModal: false,
+      showDeleteModal: false,
+      editedParticipant: null as { name: string; birthday: string; email: string; phone: string } | null,
+      deletedParticipant: null as { name: string; email: string } | null,
+    };
+  },
   methods: {
-    sortByName() {
-        this.participants.sort((a, b) => a.name.localeCompare(b.name));
+    openEditModal(participant) {
+      this.editedParticipant = { ...participant };
+      this.showEditModal = true;
     },
-    sortByBirthday() {
-        this.participants.sort((a, b) => new Date(a.birthday).getTime() - new Date(b.birthday).getTime());
+    closeEditModal() {
+      this.showEditModal = false;
     },
-    sortByBirthdayDown() {
-        this.participants.sort((a, b) => new Date(b.birthday).getTime() - new Date(a.birthday).getTime());
+    updateParticipant() {
+      const index = this.participants.findIndex(p => p.email === this.editedParticipant?.email);
+      if (index !== -1) this.participants.splice(index, 1, this.editedParticipant);
+      this.closeEditModal();
     },
-    
-}
-
+    openDeleteModal(participant) {
+      this.deletedParticipant = participant;
+      this.showDeleteModal = true;
+    },
+    closeDeleteModal() {
+      this.showDeleteModal = false;
+    },
+    confirmDeleteParticipant() {
+      const index = this.participants.findIndex(p => p.email === this.deletedParticipant?.email);
+      if (index !== -1) this.participants.splice(index, 1);
+      this.closeDeleteModal();
+    },
+    handleKeyDown(event: KeyboardEvent) {
+      if (event.key === 'Escape') {
+        if (this.showEditModal) this.closeEditModal();
+        if (this.showDeleteModal) this.closeDeleteModal();
+      }
+    },
+  },
+  mounted() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  },
+  unmounted() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  },
 });
 </script>
